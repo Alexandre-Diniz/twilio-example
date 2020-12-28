816e16f57c4cd7487838da6a05db79cb2de0b1ed
MirLih <alexandre.diniz.dias@gmail.com>
2020-12-28 20:10:35 -0300

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var accountSid = 'AC401c92051d29980474245db01e547766'
-var authToken = 'af7aaf6d3e285320f39fa500f57b2bd8'
+var accountSid = 'yyyyyyyyyyyyyy'
+var authToken = 'xxxxxxxxxxxxxxx'
 
 module.exports = {accountSid,authToken}
